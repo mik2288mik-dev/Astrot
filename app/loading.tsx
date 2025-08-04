bebeedb35a5f09c7001d92bddb530400200fad04
Mikhail Sergeevich <mik2288mik@gmail.com>
2025-08-04 15:29:09 +0300

    chore: align natal loading message

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 import CosmicLoader from "@/components/CosmicLoader";
 
 export default function Loading() {
-  return <CosmicLoader message="Загрузка Вселенной..." />;
+  return <CosmicLoader message="Подключение к космосу..." />;
 }
 
